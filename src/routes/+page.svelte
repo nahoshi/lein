8a49146e38e2a0e366e4ff8bfb11f18b6907ff30
Nahoshi <nahoshi@proton.me>
2025-12-08 19:52:47 -0300

    feat(page): Implement the index page in an elegant way.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
-<h1>I love you risia!</h1>
+<script lang="ts">
+    import Session from "$lib/components/Session.svelte";
+    import type { PageData } from "./$types";
+
+    let {
+        data,
+    }: {
+        data: PageData;
+    } = $props();
+</script>
+
+<h1 class="text-5xl font-semibold text-center">Nahoshi</h1>
+
+<div class="avatar flex w-full justify-center">
+    <div class="w-[60%] max-w-72 rounded-full">
+        <img src="/icon.webp" alt="My icon: Nanahoshi" />
+    </div>
+</div>
+
+<div>
+    <h2 class="text-2xl font-semibold text-center">Guilherme Nunes</h2>
+    <p class="text-center">
+        Apenas um louco por flutter, python, svelte e tui's
+    </p>
+</div>
+
+<div class="flex flex-col gap-2 mx-auto md:w-[70%] w-[95%] justify-center">
+    {#each data.sessions as session}
+        <Session title={session.name} links={session.links} />
+    {/each}
+</div>
