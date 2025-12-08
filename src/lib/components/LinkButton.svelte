0bbdc847ebb72647aaf10a099c5fe593479ff1e8
Nahoshi <nahoshi@proton.me>
2025-12-08 18:52:49 -0300

    feat(component): Create a LinkButton to display the Link entity.

---
+++ src/lib/components/LinkButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import type { Icon } from "../../generated/prisma/client";
+
+    let {
+        link,
+        children,
+        icon = null,
+    }: {
+        icon: Icon | null;
+        children: Snippet;
+        link: string;
+    } = $props();
+</script>
+
+<a class="w-full" href={link}>
+    <div role="alert" class="alert rounded-lg p-4">
+        {#if icon}
+            <i class="fa-{icon.type} fa-{icon.name} text-3xl"></i>
+        {/if}
+        <span class="font-semibold text-xl">{@render children?.()}</span>
+    </div>
+</a>
