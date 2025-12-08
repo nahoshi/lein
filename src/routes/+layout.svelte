e7e5cc8a0c291f413608974e490e59d5fb573e86
Nahoshi <nahoshi@proton.me>
2025-12-08 18:52:11 -0300

    feat(layout): Style the layout to center the divs to an acceptable size.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,11 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<main class="min-h-screen p-6 flex flex-col items-center">
+    <div class="w-full max-w-3xl space-y-4 flex flex-col gap-4">
+        {@render children()}
+    </div>
+</main>
