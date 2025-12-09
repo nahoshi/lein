0ea5658199e95e38b865435eaae1d1a95bf88118
Nahoshi <nahoshi@proton.me>
2025-12-09 00:34:49 -0300

    feat-css(component): Adjust the sizes for mobile devices and add the fonts.

--- src/lib/components/LinkButton.svelte
+++ src/lib/components/LinkButton.svelte
@@ -13,11 +13,13 @@
     } = $props();
 </script>
 
-<a class="w-full" href={link}>
+<a class="w-full" href={link} target="_blank">
     <div role="alert" class="alert rounded-lg p-4">
         {#if icon}
-            <i class="fa-{icon.type} fa-{icon.name} text-3xl"></i>
+            <i class="fa-{icon.type} fa-{icon.name} text-2xl"></i>
         {/if}
-        <span class="font-semibold text-xl">{@render children?.()}</span>
+        <span class="font-bold text-2xl lg:text-2xl alegreya-sans"
+            >{@render children?.()}</span
+        >
     </div>
 </a>
