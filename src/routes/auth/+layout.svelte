318e07436b7ed596a4f19b3f1f6d4f1546bbb824
Nahoshi <nahoshi@proton.me>
2025-12-10 18:39:32 -0300

    feat(layout): Create layout for auth routes.

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    let {
+        children,
+    }: {
+        children: Snippet;
+    } = $props();
+</script>
+
+<main class="w-screen h-screen flex justify-center items-center">
+    {@render children?.()}
+</main>
