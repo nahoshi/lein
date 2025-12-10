9d445cf26ad82eb423e7144f45e0b785b8ab2685
Nahoshi <nahoshi@proton.me>
2025-12-10 18:36:14 -0300

    refactor-css(layout, page): Remove CSS logic from layout to page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,4 @@
     <link rel="icon" href={favicon} />
 </svelte:head>
 
-<main class="min-h-screen p-6 flex flex-col items-center">
-    <div class="w-full max-w-3xl space-y-4 flex flex-col gap-4">
-        {@render children()}
-    </div>
-</main>
+{@render children()}
