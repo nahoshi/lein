f7af7f9c6af6f9c5d8d49ee976f5492321b138cf
Nahoshi <nahoshi@proton.me>
2025-12-11 07:51:14 -0300

    feat(component): Create an input field with error handling for a form.

---
+++ src/lib/components/auth/Input.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let {
+        title,
+        name,
+        type = "text",
+        placeholder = "",
+        value = $bindable(),
+        errors = undefined,
+    }: {
+        title: string;
+        name: string;
+        value: any;
+        type?: string;
+        errors?: Array<string>;
+        placeholder: string;
+    } = $props();
+</script>
+
+<label for={name} class="label text-sm mukta-vaani">{title}</label>
+<input
+    id={name}
+    {type}
+    class="input quattrocento {errors ? 'input-error' : ''}"
+    bind:value
+    {name}
+    {placeholder}
+/>
+{#if errors}
+    {#each errors as error}
+        <p class="label text-error text-wrap">
+            {error}
+        </p>
+    {/each}
+{/if}
