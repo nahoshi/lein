78f981f519d61e4844a03d9fc106fe1527b8dbc8
Nahoshi <nahoshi@proton.me>
2025-12-11 07:53:25 -0300

    feat(server): Implement superform validation in login.

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -3,32 +3,47 @@ import { lucia } from "$lib/server/auth.js";
 import { fail, redirect, type Actions } from "@sveltejs/kit";
 import { Argon2id } from "oslo/password";
 import type { PageServerLoad } from "./$types";
+import { superValidate } from "sveltekit-superforms";
+import { zod4 } from "sveltekit-superforms/adapters";
+import { UserLoginValidator } from "$lib/validators/user";
 
-export const load: PageServerLoad = ({ locals }) => {
-    if (locals.user) {
+export const load: PageServerLoad = async (event) => {
+    if (event.locals.user) {
         redirect(302, "/")
     }
+
+    const form = await superValidate(event, zod4(UserLoginValidator))
+
+    return { form };
 }
 
 export const actions: Actions = {
-    default: async ({ request, cookies }) => {
-        const { username, password } = Object.fromEntries(await request.formData()) as Record<string, string>
-        if (!username || !password) {
-            return fail(400, { message: "Username and password are required" });
+    default: async (event) => {
+        let { cookies } = event;
+
+        const form = await superValidate(event, zod4(UserLoginValidator))
+
+        if (!form.valid) {
+            return fail(400, { form })
         }
 
         const user = await prisma.user.findUnique({
             where: {
-                username: username
+                username: form.data.username
             }
         })
         if (!user) {
-            return fail(400, { message: "Incorrect username or password" })
+            form.errors._errors = ['Nome de usuario ou senha incorretos']
+            return fail(400, { form })
         }
 
-        const validPassword = await new Argon2id().verify(user.password, password);
+        const validPassword = await new Argon2id().verify(
+            user.password,
+            form.data.password
+        );
         if (!validPassword) {
-            return fail(400, { message: "Incorrect username or password" })
+            form.errors._errors = ['Nome de usuario ou senha incorretos']
+            return fail(400, { form })
         }
 
         const session = await lucia.createSession(user.id, [])
